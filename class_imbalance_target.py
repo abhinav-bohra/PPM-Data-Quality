47268de957c53c7def48d9549b40e098f75b8dc4
Abhinav Bohra <abhinavbohra@iitkgp.ac.in>
2022-03-05 00:25:46 +0530

    prefix class imbalance done

--- class_imbalance_target.py
+++ class_imbalance_target.py
@@ -57,22 +57,27 @@ column_names = {
 }
 
 if __name__ == "__main__":
-  fields = ["Dataset","Model","Column","Size of Dataset","Num Classes","Imbalance Ratio",\
-   "Multi-Minority/Majority","Minority Class", "Euclidean distance", "Chebyshev distance",\
- "Kullback Leibler divergence","Hellinger distance","Total variation distance","Chi-square divergence"]
-  rows = list()
   args = parser.parse_args()
   folder = args.folder
   path = f"{folder}/models/run0"
   logs = os.listdir(path)
+
+  #------------------------------------------------------------------------------------------------------------------
+  # Dataset Level
+  #------------------------------------------------------------------------------------------------------------------
+  fields = ["Dataset","Model","Column","Size of Dataset","Num Classes","Imbalance Ratio",\
+   "Multi-Minority/Majority","Minority Class", "Euclidean distance", "Chebyshev distance",\
+   "Kullback Leibler divergence","Hellinger distance","Total variation distance","Chi-square divergence"]
+  rows = list()
+  
   for log in logs:
     models = os.listdir(f"{path}/{log}")
     for model in models:
       #Concatante all target dataframes
       targets = glob.glob(f"{path}/{log}/{model}/targets*.csv")
-      files = [t.split('\\')[1] for t in targets if "duration" not in t and "setup" not in t] #Consider only categorical columns
+      target_files = [t.split('/')[-1] for t in targets if "duration" not in t and "setup" not in t] #Consider only categorical columns
       target_dfs = list()
-      for file in files:
+      for file in target_files:
         dataset_path = f"{path}/{log}/{model}/{file}"
         df = pd.read_csv(dataset_path)
         df = df.dropna()
@@ -85,9 +90,55 @@ if __name__ == "__main__":
         row = [log,model,col] + (class_imbalance_analysis(df_final,col))
         rows.append(row)
 
-  with open(f"{folder}/class_imbalance_results.csv",'w', newline='') as csvfile: 
+  #Saving results
+  with open(f"{folder}/class_imbalance_complete_results.csv",'w', newline='') as csvfile: 
       csvwriter = csv.writer(csvfile)
       csvwriter.writerow(fields) 
       csvwriter.writerows(rows)
   csvfile.close()
-print("[SUCCESS] Computed Class Imbalance successfully")
+  print("[SUCCESS] Computed Class Imbalance successfully")
+
+  #------------------------------------------------------------------------------------------------------------------
+  # Case Level
+  #------------------------------------------------------------------------------------------------------------------
+  results = dict()
+  case_fields = ["Dataset","Model","Case", "Column", "Size of Dataset","Num Classes","Imbalance Ratio",\
+   "Multi-Minority/Majority","Minority Class", "Euclidean distance", "Chebyshev distance",\
+   "Kullback Leibler divergence","Hellinger distance","Total variation distance","Chi-square divergence"]
+  for log in logs:
+    models = os.listdir(f"{path}/{log}")
+    for model in models:
+      #Concatante all target dataframes
+      targets = glob.glob(f"{path}/{log}/{model}/targets*.csv")
+      target_files = [t.split('/')[-1] for t in targets if "duration" not in t and "setup" not in t] #Consider only categorical columns
+      target_dfs = list()
+      for file in target_files:
+        dataset_path = f"{path}/{log}/{model}/{file}"
+        df = pd.read_csv(dataset_path)
+        df = df.dropna()
+        df = df.rename(columns={df.columns[0]:column_names[file]})
+        target_dfs.append(df)
+      
+      df_final = pd.concat(target_dfs, axis=1)
+      all_cols = df_final.columns.tolist()
+
+      case_len = pd.read_csv(f"{path}/{log}/{model}/features.csv")['case_len']
+      assert len(case_len) == len(df_final.index)
+      df_final['case_len'] = case_len
+      groups = df_final.groupby('case_len')
+      case_len_grps = list(groups.groups.keys())
+      case_df = pd.DataFrame(columns=case_fields)
+      i=0
+      for col in all_cols:
+        for case in case_len_grps:
+          case_len_df = groups.get_group(case)
+          case_df.loc[i] = [log,model,case,col] + (class_imbalance_analysis(case_len_df,col))
+          i=i+1
+      
+      results[f'{log}_{model}'] = case_df
+
+  #Saving results
+  with pd.ExcelWriter(f'{folder}/class_imbalance_case_results.xlsx') as writer:
+    for key in results:
+        results[key].to_excel(writer, sheet_name=key, index=False)
+  print("[SUCCESS] Computed Case Level Class Imbalance successfully")
