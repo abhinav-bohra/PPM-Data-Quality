25c1bb1d909c09ff3a554f11b4962550e9b75313
Abhinav Bohra <abhinavbohra@iitkgp.ac.in>
2022-06-27 20:35:14 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ git config --global username "abhinav-bohra"
 git add .
 git status
 git commit -m "Outlier Experiment"
-git push https://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@github.com/abhinav-bohra/PPM-Data-Quality.git
+git push 
